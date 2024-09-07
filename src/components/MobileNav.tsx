0a9925dd4c0af2f3bfe9877f548ce87dfc643944
Vivor Sharma <vivorsharma2022@gmail.com>
2024-09-07 21:33:17 +0530

    first commit

---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,43 @@
+import { CircleUserRound, Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
+// import {  } from "@radix-ui/react-separator";
+import { Button } from "./ui/button";
+import { Separator } from "./ui/separator";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
+
+const MobileNav = () => {
+    const { isAuthenticated, user, loginWithRedirect } = useAuth0()
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Menu className="text-orange-500" />
+            </SheetTrigger>
+            <SheetContent className="space-y-3">
+                <SheetTitle>
+                    {isAuthenticated ? (
+                        <span className="flex items-center font-bold gap-2">
+                            <CircleUserRound className="text-orange-500" />
+                            {user?.email}
+                        </span>
+                    ) : (
+                        <span>Welcome to MernEats.com</span>
+                    )}
+
+                </SheetTitle>
+                <Separator />
+                <SheetDescription className="flex flex-col gap-4">
+                    {isAuthenticated ? (<MobileNavLinks />
+                    ) : (
+                        <Button className="flex-1 font-bold bg-orange-500"
+                        onClick={() => loginWithRedirect()}
+                        >Log In</Button>
+                    )}
+
+                </SheetDescription>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+export default MobileNav;
